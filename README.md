adbf9a407dc659886e92676e185b52b50da2f995
fordes <ec.cheng.16888@gmail.com>
2023-07-14 23:47:43 +0800

    feat: 🎉 init

---
+++ README.md
@@ -0,0 +1,52 @@
+# spotify-loginer
+
+利用 Python 和 GithubAction 自动化定时登录 Spotify，解决14天使用限制.
+
+## 环境要求
+
+- Python3+
+- Windows10+
+- Chrome
+
+## 本地运行
+
+1. 安装 Chrome 及 ChromeDriver 
+```powershell
+choco install googlechrome -y
+choco install chromedriver -y
+```
+
+2. 安装 Python3
+```powershell
+choco install python3 -y
+```
+
+3. 安装依赖
+```powershell
+python -m pip install --upgrade pip
+pip install selenium argparse
+
+```
+4. 运行
+```powershell
+python login.py -e <email> -p <password>
+```
+
+命令行参数说明：
+```bash
+  -h, --help            show this help message and exit
+  -e EMAIL, -u EMAIL, --email EMAIL
+                        Spotify account email
+  -p PASSWORD, --password PASSWORD
+                        Spotify account password
+```
+
+## GithubAction 部署
+
+1. Fork 本仓库
+2. 在仓库的 Settings -> Secrets 中添加以下 Secrets:
+   - `SPOTIFY_EMAIL`: Spotify 账号邮箱
+   - `SPOTIFY_PASSWORD`: Spotify 账号密码
+3. 在仓库的 Actions 中启用 GithubAction, 选中 `Run Spotify Loginer`，点击 `Run workflow` 手动运行一次
+4. Enjoy it!
+
---
+++ spotify_loginer.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import sys, argparse, time
+
+
+def main():
+   parser = argparse.ArgumentParser()
+   parser.add_argument("-e", "-u", "--username", "--email", help="Spotify account email", required=True)
+   parser.add_argument("-p", "--password", help="Spotify account password", required=True)
+   args = parser.parse_args()
+
+   if not args.email or not args.password:
+      print("Error: email and password cannot be empty")
+      exit(1)
+
+   chrome_options = Options()
+   chrome_options.add_argument('--headless')
+   chrome_options.add_argument('--no-sandbox')
+   chrome_options.add_argument('--disable-dev-shm-usage')
+   driver = webdriver.Chrome()
+   driver.get("https://accounts.spotify.com/en/login?continue=https:%2F%2Fopen.spotify.com%2F")
+   
+   # Wait for the page to load
+   wait = WebDriverWait(driver, 10)
+   wait.until(EC.presence_of_element_located((By.ID, "login-button")))
+
+   # Login
+   element = driver.find_element(By.ID, 'login-username')
+   element.send_keys(args.email)
+   element = driver.find_element(By.ID, "login-password")
+   element.send_keys(args.password)
+   element.send_keys(Keys.RETURN)
+
+   # Wait for the page to load
+   wait = WebDriverWait(driver, 10)
+   wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "figure[data-testid='user-widget-avatar']")))
+   time.sleep(5)
+
+   # Check if login was successful
+   if driver.current_url.startswith('https://open.spotify.com/'):
+      print('Login successful')
+      sys.exit(0)
+   else:
+      print('Login failed or timed out')
+      sys.exit(1)
+
+if __name__ == "__main__":
+   main()
